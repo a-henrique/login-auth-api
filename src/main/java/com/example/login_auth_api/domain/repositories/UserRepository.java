9ffca8ab8985e0d6ce5e36716e7b8bb4cddd7800
a-henrique <audrinh@gmail.com>
2025-04-21 19:21:15 -0300

    Creating main archives and defining a generation token function

---
+++ src/main/java/com/example/login_auth_api/domain/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.login_auth_api.domain.repositories;
+
+import com.example.login_auth_api.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User,String> {
+}
